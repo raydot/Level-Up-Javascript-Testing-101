eaab3bf0113fc0438149fe7d335fb8789647aefc
Dave Kanter <moatgoat@hotmail.com>
2020-06-27 17:05:57 -0700

    Starting #6 Mocking Modules.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,1 @@
-export const add = (x, y) => {
-  return x + y;
-};
-
-// export const add = (x, y) => x + y // same thing
-
-export const total = (shipping, subTotal) => {
-  return '$' + add(shipping, subTotal);
-};
+import { add } from './add';
